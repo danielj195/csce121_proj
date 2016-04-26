40e83f42c840c7b56c44d523039510604c5b7c12
Hunter Lukken <eightch@mac.com>
2016-04-26 18:42:22 -0500

    Stuff I have to add to sync

--- fltk/distance.cpp
+++ fltk/distance.cpp
@@ -14,7 +14,7 @@ double H = 600;
 
 double y_to_latitude(double y) {
 
-	return (2*(atan(exp(y*2*M_PI/W))-(M_PI/4)))*(180/M_PI);
+	return (2*(atan(exp(y*2*M_PI/W))-(M_PI/4)))*(180/M_PI); //formula came from https://en.wikipedia.org/wiki/Latitude
 	
 }
 
@@ -31,25 +31,7 @@ double latitude_to_y(double lat) {
 double longitude_to_x(double lon) {
     return deg_rad(lon)*(W/(2*M_PI)); //longitude from degrees to meters
 }
-/*
-double distance_x(double x1, double x2){
-    double w = x1-x2;
-    double z = 360-(x1-x2);
-    if (w<z)
-        return z;
-    else
-        return w;// returns x distance in degrees
-}
 
-double distance_y(double y1, double y2){
-    double x = y1-y2;
-    double c = 180-(y1-y2);
-    if (x<c)
-        return x;
-    else
-        return c;// returns y distance in degrees
-}
-*/
 double arc_length(double lon1, double lat1, double lon2, double lat2){
     double phi1 = deg_rad(lat1);
     double phi2 = deg_rad(lat2); //converting degrees to radians
